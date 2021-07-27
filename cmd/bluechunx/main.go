6235de31201e98378f8954b2459ace2d4e59e2bd
Mike Allred <mikejallred@gmail.com>
2021-07-26 21:49:08 -0400

    More stuff.

--- cmd/bluechunx/main.go
+++ cmd/bluechunx/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"tinygo.org/x/bluetooth"
@@ -14,7 +18,9 @@ type BxMessage struct {
 	Addr string
 	LName string
 	Rssi int16
+	Epoch int64
 }
+var ctx = context.Background()
 var adapter = bluetooth.DefaultAdapter
 
 func valmaster(mofo map[string]string) []string {
@@ -23,6 +29,16 @@ func valmaster(mofo map[string]string) []string {
 		vals = append(vals, v)
 	}
 	return vals
+}
+
+func RedisClient() *redis.Client {
+	opt, err := redis.ParseURL("redis://" + os.Getenv("BLUECHUNX_REDIS_URL"))
+	if err != nil {
+		fmt.Printf("redis parseurl fail ", err)
+	}
+	//rdb := redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(opt)
+	return rdb
 }
 
 func main() {
@@ -35,7 +51,9 @@ func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
     log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
 	adapter.Enable()
-	log.Info().Msg("Starting scan...")
+	rdb := RedisClient()
+	pong, err := rdb.Ping(ctx).Result()
+	log.Info().Str("redisPing", pong).Msg("Starting scan...")
     err = adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 		addr := device.Address.String()
 		rssi := device.RSSI
@@ -44,13 +62,25 @@ func main() {
 			bxAll[addr] = 1 
 			if addr != "" {
 				//rssiStr := strconv.FormatInt(int64(rssi), 10)
-				m := BxMessage{addr, localname, rssi}
-				//fmt.Printf("debu %v\n", m)
+				now := time.Now()
+				epoch := now.Unix()
+				m := BxMessage{addr, localname, rssi, epoch}
 				jsonBytes, err := json.Marshal(m)
 				if err != nil {
 					log.Error().Str("err", "x").Msg("json marshal failed")
 				}
 				//log.Debug().Str("a", addr).Str("r", rssiStr).Str("n", localname).Msg(strconv.Itoa(len(bxAll)))
+
+				_, errR := rdb.HMSet(ctx, "bluechunx:wyvern", addr, jsonBytes).Result()
+				//fmt.Printf("debu %v\n", hash)
+				if errR != nil {
+					log.Error().Str("err", "err").Msg("redis hmset failed")
+				}
+				errRp := rdb.Publish(ctx, "bluechunx", jsonBytes).Err()
+				if errRp != nil {
+					log.Error().Str("err", "err").Msg("redis publish failed")
+				}
+
 		    	log.Debug().RawJSON("j", jsonBytes).Msg(strconv.Itoa(len(bxAll)))
 			}
 		}
